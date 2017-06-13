71c3688ee3116960380c88524cf1472f19a9edfc
Octavian Dima <hello@wopss.ro>
2017-06-13 14:00:38 +0300

    Add LOG_* macros

--- Source/RenHook/RenHook.hpp
+++ Source/RenHook/RenHook.hpp
@@ -10,3 +10,27 @@
 #include <vector>
 
 #include <Windows.h>
+
+#ifdef CUSTOM_LOGGER
+#include CUSTOM_LOGGER
+#endif
+
+#ifdef _DEBUG
+#ifndef LOG_DEBUG
+#define LOG_DEBUG std::wcout << L"[DEBUG] "
+#endif
+#endif
+
+#ifndef LOG_INFO
+#define LOG_INFO std::wcout << L"[INFO] "
+#endif
+
+#ifndef LOG_WARNING
+#define LOG_WARNING std::wcout << L"[WARNING] "
+#endif
+
+#ifndef LOG_ERROR
+#define LOG_ERROR std::wcout << L"[ERROR] "
+#endif
+
+#include <RenHook/Hooks/Hooks.hpp>
