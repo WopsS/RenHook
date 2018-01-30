e03cd70e608ea7e535f1a3af49a2a1d48e7b431a
Octavian Dima <hello@wopss.ro>
2018-01-30 15:39:45 +0100

    Use just x86 architecture for Capstone

--- Dependencies/Capstone/premake5.lua
+++ Dependencies/Capstone/premake5.lua
@@ -1,5 +1,5 @@
 project("Capstone")
-    defines({ "CAPSTONE_X86_ATT_DISABLE_NO", "CAPSTONE_DIET_NO", "CAPSTONE_X86_REDUCE_NO", "CAPSTONE_HAS_ARM", "CAPSTONE_HAS_ARM64", "CAPSTONE_HAS_MIPS", "CAPSTONE_HAS_POWERPC", "CAPSTONE_HAS_SPARC", "CAPSTONE_HAS_SYSZ", "CAPSTONE_HAS_X86", "CAPSTONE_HAS_XCORE", "CAPSTONE_USE_SYS_DYN_MEM", "WIN32", "_DEBUG", "_LIB" })
+    defines({ "CAPSTONE_HAS_X86", "CAPSTONE_USE_SYS_DYN_MEM", "WIN32" })
     kind("StaticLib")
     language("C")
     removedefines({ "_CRT_SECURE_NO_WARNINGS" })
@@ -8,14 +8,6 @@ project("Capstone")
         targetdir(buildpath())
     end
 
-    filter({ "configurations:Debug" })
-        symbols("On")
-
-    filter({ "configurations:Release" })
-        symbols("Off")
-
-    filter({})
-
     includedirs({ "Capstone/include" })
 
-    files({  "Capstone/arch/**.c", "Capstone/cs.c", "Capstone/MCInst.c", "Capstone/MCInstrDesc.c", "Capstone/MCRegisterInfo.c", "Capstone/SStream.c" , "Capstone/utils.c" })
+    files({  "Capstone/arch/X86/**.c", "Capstone/cs.c", "Capstone/MCInst.c", "Capstone/MCInstrDesc.c", "Capstone/MCRegisterInfo.c", "Capstone/SStream.c" , "Capstone/utils.c" })
