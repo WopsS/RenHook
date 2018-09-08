b706742fcd47c148d4f1a6b0388c2174e7e16a9e
Octavian Dima <hello@wopss.ro>
2018-09-08 14:21:06 +0200

    Add inital files

---
+++ source/renhook/version.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdint>
+
+namespace dodbm
+{
+    namespace renhook
+    {
+        constexpr const char* as_string()
+        {
+            return "1.0.0";
+        }
+
+        constexpr uint32_t as_int()
+        {
+            return 10000;
+        }
+
+        constexpr uint8_t get_major()
+        {
+            return 1;
+        }
+
+        constexpr uint8_t get_minor()
+        {
+            return 0;
+        }
+
+        constexpr uint8_t get_patch()
+        {
+            return 0;
+        }
+    }
+}
