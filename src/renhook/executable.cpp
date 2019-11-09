118a155d708a2dcbe8454368144d57f8c6a9a977
Octavian Dima <hello@wopss.net>
2019-11-09 06:07:59 +0100

    Remove std::call_once calls in "executable" namespace

--- src/renhook/executable.cpp
+++ src/renhook/executable.cpp
@@ -1,17 +1,9 @@
 #include <renhook/executable.hpp>
 
-#include <mutex>
-
 #include <Windows.h>
-#include <winnt.h>
 
 namespace
 {
-    uintptr_t base_address;
-
-    uintptr_t code_base_address;
-    uintptr_t code_end_address;
-
     PIMAGE_NT_HEADERS get_nt_header()
     {
         auto base_address = renhook::executable::get_base_address();
@@ -22,35 +14,18 @@ namespace
 
 uintptr_t renhook::executable::get_base_address()
 {
-    static std::once_flag once_flag;
-    std::call_once(once_flag, []()
-    {
-        base_address = reinterpret_cast<uintptr_t>(GetModuleHandle(nullptr));
-    });
-
+    static auto base_address = reinterpret_cast<uintptr_t>(GetModuleHandle(nullptr));
     return base_address;
 }
 
 uintptr_t renhook::executable::get_code_base_address()
 {
-    static std::once_flag once_flag;
-    std::call_once(once_flag, []()
-    {
-        auto nt_header = get_nt_header();
-        code_base_address = get_base_address() + nt_header->OptionalHeader.BaseOfCode;
-    });
-
+    static auto code_base_address = get_base_address() + get_nt_header()->OptionalHeader.BaseOfCode;
     return code_base_address;
 }
 
 uintptr_t renhook::executable::get_code_end_address()
 {
-    static std::once_flag once_flag;
-    std::call_once(once_flag, []()
-    {
-        auto nt_header = get_nt_header();
-        code_end_address = get_code_base_address() + nt_header->OptionalHeader.SizeOfCode;
-    });
-
+    static auto code_end_address = get_code_base_address() + get_nt_header()->OptionalHeader.SizeOfCode;
     return code_end_address;
 }
