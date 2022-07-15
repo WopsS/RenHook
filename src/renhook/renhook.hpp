cba7a38a7386f675c5280e549a7c6ec022174b1d
Octavian Dima <3403191+WopsS@users.noreply.github.com>
2022-07-15 20:48:29 +0200

    Split files in "include" and "src" directories

--- src/renhook/renhook.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include <renhook/hooks/prologue_hook.hpp>
--- tests/utils.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-#include <renhook/utils.hpp>
-
-TEST_CASE("utils")
-{
-    REQUIRE(renhook::utils::calculate_displacement(100, 50, 5) == -55);
-    REQUIRE(renhook::utils::calculate_displacement(30, 60, 5) == 25);
-}
